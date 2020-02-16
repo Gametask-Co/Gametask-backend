7a84244b97a543d24c7b4c8d08ccbf2614d0f9ca
Henrique Albuquerque <eullerhenrique95@hotmail.com>
2020-02-16 18:35:57 -0300

    Adição rota principal

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import UserController from './app/controllers/UserController';
 import ProjectController from './app/controllers/ProjectController';
 
 import authMiddleware from './app/middlewares/auth';
+import soon from './views';
 
 const routes = new Router();
 
@@ -20,7 +21,11 @@ routes.post('/user/auth', authMiddleware, UserController.auth);
 routes.put('/user/', UserController.update);
 
 // rota de teste (exemplo de como usar middle de autenticacao)
-
 routes.get('/project', authMiddleware, ProjectController.test);
+
+// rota principal ( Em breve )
+routes.get('/', (req, res) => {
+    res.send(soon)
+});
 
 export default routes;
