9f1935b303d5b7c7ecf830544e4c9b6191376604
Breno Alves <breno.alves@academico.ifrn.edu.br>
2020-10-12 20:43:35 -0300

    authentication

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from 'express';
+
 import userRouter from './user.routes';
+import sessionRouter from './session.routes';
 
 const routes = Router();
 
+routes.use('/session', sessionRouter);
 routes.use('/user', userRouter);
 
 export default routes;
