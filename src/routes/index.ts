56c4abed0315910bd4b8250ca2d4509afd4c843f
BRENO PEREIRA ALVES <breno.alves@academico.ifrn.edu.br>
2020-10-18 22:07:02 -0300

    Resolve "Criar rotas e services para cadastrar e excluir um Student de uma Subject"

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,17 @@
 import { Router } from 'express';
 
-import userRouter from './user.routes';
-import sessionRouter from './session.routes';
-import studentRouter from './student.routes';
-import teacherRouter from './teacher.routes';
-import subjectRouter from './subject.routes';
+import SessionRouter from './session.routes';
+import UserRouter from './user.routes';
+import StudentRouter from './student.routes';
+import TeacherRouter from './teacher.routes';
+import SubjectRouter from './subject.routes';
 
 const routes = Router();
 
-routes.use('/session', sessionRouter);
-routes.use('/user', userRouter);
-routes.use('/student', studentRouter);
-routes.use('/teacher', teacherRouter);
-routes.use('/subject', subjectRouter);
+routes.use('/session', SessionRouter);
+routes.use('/user', UserRouter);
+routes.use('/student', StudentRouter);
+routes.use('/teacher', TeacherRouter);
+routes.use('/subject', SubjectRouter);
 
 export default routes;
