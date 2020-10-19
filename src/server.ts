5ea5eab7ca59355515ed0092854052f1cc94ebdd
BRENO PEREIRA ALVES <breno.alves@academico.ifrn.edu.br>
2020-10-19 20:08:15 -0300

    Cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import 'reflect-metadata';
 
 import express from 'express';
-import routes from './routes/index';
-
+import cors from 'cors';
 import './database';
+
+import routes from './routes/index';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
