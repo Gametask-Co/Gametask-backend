3dbcb11cedd4ff7222a2f0a3cf81e8fc3d77d4d7
Breno Alves <breno550@hotmail.com>
2020-11-22 15:59:09 -0300

    create milestone service and tests

---
+++ src/modules/subjects/services/CreateMilestoneService.ts
@@ -0,0 +1,45 @@
+import AppError from '@shared/errors/AppError';
+import { inject, injectable } from 'tsyringe';
+import CreateMilestoneDTO from '../dtos/CreateMilestoneDTO';
+import Milestone from '../infra/typeorm/entities/Milestone';
+import IMilestonesRepository from '../repositories/IMilestoneRepository';
+import ISubjectsRepository from '../repositories/ISubjectsRepository';
+
+@injectable()
+class CreateMilestoneService {
+  private milestonesRepository: IMilestonesRepository;
+
+  private subjectsRepository: ISubjectsRepository;
+
+  constructor(
+    @inject('MilestonesRepository')
+    milestonesRepository: IMilestonesRepository,
+    @inject('SubjectsRepository')
+    subjectsRepository: ISubjectsRepository,
+  ) {
+    this.milestonesRepository = milestonesRepository;
+    this.subjectsRepository = subjectsRepository;
+  }
+
+  public async execute({
+    name,
+    subject_id,
+    description,
+  }: CreateMilestoneDTO): Promise<Milestone> {
+    const findSubject = this.subjectsRepository.findById(subject_id);
+
+    if (!findSubject) {
+      throw new AppError('Subject not found');
+    }
+
+    const milestone = this.milestonesRepository.create({
+      name,
+      description,
+      subject_id,
+    });
+
+    return milestone;
+  }
+}
+
+export default CreateMilestoneService;
