e1885cebc63ae1773d86a145ed7a82e0dd5031d3
Breno Alves <breno550@hotmail.com>
2020-11-22 18:09:09 -0300

    user avatar upload

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,13 +1,27 @@
 import { Router } from 'express';
 
+import multer from 'multer';
+import uploadConfig from '@config/upload';
+
 import UserController from '@modules/users/infra/controller/UsersController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
+import UserAvatarController from '../../controller/UserAvatarController';
 
 const userRouter = Router();
 const userController = new UserController();
 
+const userAvatarController = new UserAvatarController();
+const upload = multer(uploadConfig);
+
 userRouter.post('/', userController.create);
 userRouter.get('/', ensureAuthenticated, userController.index);
+
+userRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  userAvatarController.update,
+);
 
 // userRouter.delete('/', ensureAuthenticated, async (request, response) => {
 //   try {
