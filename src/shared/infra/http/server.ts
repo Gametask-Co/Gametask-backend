02e9d00669756d8a55e832824f9fc9a20e8e8804
Breno Alves <breno550@hotmail.com>
2020-11-23 17:40:34 -0300

    hot fix: small typo

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -25,7 +25,7 @@ app.use(routes);
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   if (err instanceof AppError) {
     return response.status(err.statusCode).json({
-      stauts: 'error',
+      status: 'error',
       message: err.message,
     });
   }
