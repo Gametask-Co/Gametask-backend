82d88b7d2e4b9b8af19218f1be96145fb9463a06
Breno Alves <breno550@hotmail.com>
2020-11-29 13:34:18 -0300

    template

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -6,6 +6,8 @@ import TeachersRouter from '@modules/teachers/infra/http/routes/teachers.routes'
 import StudentsRouter from '@modules/students/infra/http/routes/students.routes';
 import SubjectsRouter from '@modules/subjects/infra/http/routes/subjects.routes';
 
+import path from 'path';
+
 const routes = Router();
 
 routes.use('/sessions', SessionRouter);
@@ -13,5 +15,9 @@ routes.use('/users', UserRouter);
 routes.use('/students', StudentsRouter);
 routes.use('/teachers', TeachersRouter);
 routes.use('/subjects', SubjectsRouter);
+
+routes.use('/', (req, res) => {
+  res.sendFile(path.resolve('src/views', 'index.html'));
+});
 
 export default routes;
