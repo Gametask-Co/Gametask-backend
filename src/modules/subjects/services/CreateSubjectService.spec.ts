f9f140aaef7d3e13c0e1304ab19b790b3ffa73c4
Breno Alves <breno550@hotmail.com>
2021-01-16 21:54:32 -0300

    createsubject tests

--- src/modules/subjects/services/CreateSubjectService.spec.ts
+++ src/modules/subjects/services/CreateSubjectService.spec.ts
@@ -1,47 +1,46 @@
 import 'reflect-metadata';
 
+import connection from '@shared/helper/connection';
+
 import Teacher from '@modules/teachers/infra/typeorm/entities/Teacher';
 import CreateTeacherService from '@modules/teachers/services/CreateTeacherService';
 import CreateSubjectService from '@modules/subjects/services/CreateSubjectService';
+import { createTeacher } from '@shared/helper/testHelper';
+import TeachersRepository from '@modules/teachers/infra/typeorm/repositories/TeachersRepository';
+// import FakeSubjectsRepository from '@modules/subjects/repositories/fakes/fakeSubjectsRepository';
+// import FakeTeachersRepository from '@modules/teachers/repositories/fakes/fakeTeachersRepository';
+// import FakeUsersRepository from '@modules/users/repositories/fakes/fakeUsersRepository';
 
-import FakeSubjectsRepository from '@modules/subjects/repositories/fakes/fakeSubjectsRepository';
-import FakeTeachersRepository from '@modules/teachers/repositories/fakes/fakeTeachersRepository';
-import FakeUsersRepository from '@modules/users/repositories/fakes/fakeUsersRepository';
+import SubjectsRepository from '@modules/subjects/infra/typeorm/repositories/SubjectsRepository';
 
 import ITeachersRepository from '@modules/teachers/repositories/ITeachersRepository';
-import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
 describe('CreateSubject', () => {
-  let fakeUsersRepository: IUsersRepository;
-  let fakeTeachersRepository: ITeachersRepository;
+  let teachersRepository: ITeachersRepository;
   let teacher: Teacher;
 
   beforeAll(async () => {
-    fakeUsersRepository = new FakeUsersRepository();
-    fakeUsersRepository.create({
+    await connection.create();
+    teachersRepository = new TeachersRepository();
+
+    teacher = await createTeacher({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123123',
     });
+  });
 
-    const user = await fakeUsersRepository.findByEmail('johndoe@example.com');
-
-    fakeTeachersRepository = new FakeTeachersRepository(fakeUsersRepository);
-
-    const createTeacher = new CreateTeacherService(
-      fakeTeachersRepository,
-      fakeUsersRepository,
-    );
-
-    teacher = await createTeacher.execute(user.id);
+  afterAll(async () => {
+    await connection.clear();
+    await connection.close();
   });
 
   it('Should create a new subject', async () => {
-    const fakeSubjectsRepository = new FakeSubjectsRepository();
+    const subjectsRepository = new SubjectsRepository();
 
     const createSubject = new CreateSubjectService(
-      fakeSubjectsRepository,
-      fakeTeachersRepository,
+      subjectsRepository,
+      teachersRepository,
     );
 
     const subject = await createSubject.execute({
